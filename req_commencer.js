03360609c5d8346c8e27575eb6d7a9662257d4d8
Namory <nnamorykanate5@gmail.com>
2024-07-28 16:51:02 +0200

    Ajouter la fonctionnalité joué

--- req_commencer.js
+++ req_commencer.js
@@ -1,31 +1,30 @@
 const fs = require("fs");
 const nunjucks = require("nunjucks");
-const mots = require("./data.json");
+const data = require("./data.json");
 const utile = require("./utiles");
 
 const trait = function (req, res, query) {
   let marqueurs;
   let page;
 
-  let indexAleatoire = Math.floor(Math.random() * mots.mots.length); // chercher l'index du mot
-  let motAleatoire = mots.mots[indexAleatoire];
+  let indexAleatoire = Math.floor(Math.random() * data.mots.length); // chercher l'index du mot
+  let motAleatoire = data.mots[indexAleatoire];
 
   //lecture de mon ficher Json
   contenu_fichier = fs.readFileSync("motAleatoireGenerate.json", "utf-8");
   motgenerate = JSON.parse(contenu_fichier);
-
+  
   //écrire dans mon ficher Json
   motgenerate.mots = motAleatoire;
-  motgenerate.motSearch = utile.spacemot(motAleatoire).join(" ");
+  motgenerate.motSearch = utile.spacemot(motAleatoire);
   contenu_fichier = JSON.stringify(motgenerate);
   fs.writeFileSync("motAleatoireGenerate.json", contenu_fichier, "utf-8");
-
-  console.log(contenu_fichier);
+ console.log(contenu_fichier);
   
   // LIRE ET RENDRE LE FICHIER HTML AVEC NUNJUCKS
   page = fs.readFileSync("modele_accueil.html", "utf-8");
   marqueurs = {};
-  marqueurs.mot = motgenerate.motSearch;
+  marqueurs.mot = motgenerate.motSearch.join('');
   marqueurs.size = motAleatoire.length;
   page = nunjucks.renderString(page, marqueurs);
 
--- req_jouer.js
+++ req_jouer.js
@@ -1,22 +1,40 @@
 const fs = require("fs");
 const nunjucks = require("nunjucks");
 
+
 const trait = function (req, res, query) {
   let marqueurs;
   let page;
-  let answer =query.mot ;
-  console.log(answer);
- 
-  // LIRE ET RENDRE LE FICHIER HTML AVEC NUNJUCKS
-  //page = fs.readFileSync("modele_accueil.html", "utf-8");
-  //marqueurs = {};
-  //marqueurs.mot = answer;
-  //marqueurs.size = motAleatoire.length;
-  //page = nunjucks.renderString(page, marqueurs);
-/*
+  let html ='';
+  const answer = query.mot;
+  
+  contenu_fichier = fs.readFileSync("motAleatoireGenerate.json", "utf-8");
+  motgenerate = JSON.parse(contenu_fichier);
+
+  if(motgenerate.mots.length ==answer.length){
+    for (let i=0; i<answer.length; i++) {
+      if (motgenerate.mots[i]==answer[i]) {
+        motgenerate.motSearch[i]=answer[i];
+      }
+    }
+  }else {
+    html += '<span style="color:red;">taille du mot incorrect</span>';
+  }
+  contenu_fichier = JSON.stringify(motgenerate);
+  fs.writeFileSync("motAleatoireGenerate.json", contenu_fichier, "utf-8");
+ console.log(contenu_fichier);
+
+
+ page = fs.readFileSync("modele_accueil.html", "utf-8");
+ marqueurs = {};
+ marqueurs.mot = motgenerate.motSearch.join('');
+ marqueurs.size = motgenerate.mots.length;
+ marqueurs.error = html;
+ page = nunjucks.renderString(page, marqueurs);
+
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write(page);
-  res.end();*/
+  res.end();
 };
 
 module.exports = trait;
